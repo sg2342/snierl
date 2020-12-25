9261da81c6e591f2e8fa7bda90e60dd47fd63896
Stefan Grundmann <sg2342@googlemail.com>
2020-12-25 06:20:52 +0000

    _acceptor_sup: start  childs for every listen socket

--- src/snierl_acceptor_sup.erl
+++ src/snierl_acceptor_sup.erl
@@ -14,8 +14,9 @@ start_link() -> supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 init([]) ->
     Flags = #{ strategy => one_for_one },
     {ok, NumAcceptors} = application:get_env(num_acceptors),
+    {ok, L} = application:get_env(listen),
     Specs = [ #{ id => Id
 	       , start => { snierl_acceptor, start_link, [Id] }
 	       , modules => [ snierl_acceptor ] }
-	      || Id <- lists:seq(1, NumAcceptors) ],
+	      || Id <- lists:seq(1, NumAcceptors * length(L)) ],
     {ok, {Flags, Specs}}.
