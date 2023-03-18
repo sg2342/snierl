6dbc46aea235358a91c0aa749846fbf302737238
Stefan Grundmann <sg2342@googlemail.com>
2023-03-18 01:08:51 +0000

    _crts: elvis: fix atom_naming_convention

--- src/snierl_crts.erl
+++ src/snierl_crts.erl
@@ -7,7 +7,7 @@
 -spec extension(tuple(), binary()) -> binary() | false.
 extension(OID, DER) ->
     OtpC = public_key:pkix_decode_cert(DER, otp),
-    TBS = OtpC#'OTPCertificate'.tbsCertificate,
+    TBS = OtpC#'OTPCertificate'.'tbsCertificate',
     Extensions = TBS#'OTPTBSCertificate'.extensions,
     extension1(lists:keyfind(OID, 2, Extensions)).
 
@@ -23,9 +23,9 @@ of_pem(PemBin, Host) ->
             [C || {'Certificate', C, _} <- public_key:pem_decode(PemBin)]
         ),
     OtpCert = public_key:pkix_decode_cert(Cert, otp),
-    TBSCert = OtpCert#'OTPCertificate'.tbsCertificate,
+    TBSCert = OtpCert#'OTPCertificate'.'tbsCertificate',
     {'Validity', _, NotAfterStr} = TBSCert#'OTPTBSCertificate'.validity,
-    Expires = pubkey_cert:time_str_2_gregorian_sec(NotAfterStr),
+    Expires = pubkey_cert:'time_str_2_gregorian_sec'(NotAfterStr),
     {Expires, Cert, CaCerts}.
 
 -define(ALPN_EXT_ID, {1, 3, 6, 1, 5, 5, 7, 1, 31}).
@@ -34,11 +34,11 @@ of_pem(PemBin, Host) ->
     DERCert :: binary().
 alpn(Key, Host, KeyAuth) ->
     Subject =
-        {rdnSequence, [
+        {'rdnSequence', [
             [
                 #'AttributeTypeAndValue'{
                     type = ?'id-at-commonName',
-                    value = {printableString, Host}
+                    value = {'printableString', Host}
                 }
             ]
         ]},
@@ -50,7 +50,7 @@ alpn(Key, Host, KeyAuth) ->
         ],
     TBS = #'OTPTBSCertificate'{
         version = v3,
-        serialNumber = 15,
+        'serialNumber' = 15,
         signature = #'SignatureAlgorithm'{
             algorithm = ?'sha256WithRSAEncryption',
             parameters = 'NULL'
@@ -58,29 +58,29 @@ alpn(Key, Host, KeyAuth) ->
         issuer = Subject,
         subject = Subject,
         validity = #'Validity'{
-            notBefore = {generalTime, NotBefore},
-            notAfter = {generalTime, NotAfter}
+            'notBefore' = {'generalTime', NotBefore},
+            'notAfter' = {'generalTime', NotAfter}
         },
-        subjectPublicKeyInfo = #'OTPSubjectPublicKeyInfo'{
+        'subjectPublicKeyInfo' = #'OTPSubjectPublicKeyInfo'{
             algorithm = #'PublicKeyAlgorithm'{
                 algorithm = ?rsaEncryption,
                 parameters = 'NULL'
             },
-            subjectPublicKey = #'RSAPublicKey'{
+            'subjectPublicKey' = #'RSAPublicKey'{
                 modulus = Key#'RSAPrivateKey'.modulus,
-                publicExponent = Key#'RSAPrivateKey'.publicExponent
+                'publicExponent' = Key#'RSAPrivateKey'.'publicExponent'
             }
         },
         extensions = [
             #'Extension'{
-                extnID = ?ALPN_EXT_ID,
+                'extnID' = ?ALPN_EXT_ID,
                 critical = true,
-                extnValue = <<4, 32, KeyAuth/binary>>
+                'extnValue' = <<4, 32, KeyAuth/binary>>
             },
             #'Extension'{
-                extnID = ?'id-ce-subjectAltName',
+                'extnID' = ?'id-ce-subjectAltName',
                 critical = false,
-                extnValue = [{dNSName, Host}]
+                'extnValue' = [{'dNSName', Host}]
             }
         ]
     },
@@ -93,17 +93,17 @@ csr(Key, Host) ->
         type = ?'pkcs-9-at-extensionRequest',
         values =
             [
-                {asn1_OPENTYPE,
+                {'asn1_OPENTYPE',
                     public_key:der_encode(
                         'ExtensionRequest',
                         [
                             #'Extension'{
-                                extnID = ?'id-ce-subjectAltName',
+                                'extnID' = ?'id-ce-subjectAltName',
                                 critical = false,
-                                extnValue =
+                                'extnValue' =
                                     public_key:der_encode(
                                         'SubjectAltName',
-                                        [{dNSName, Host}]
+                                        [{'dNSName', Host}]
                                     )
                             }
                         ]
@@ -112,25 +112,25 @@ csr(Key, Host) ->
     },
     CsrI = #'CertificationRequestInfo'{
         version = v1,
-        subject = {rdnSequence, []},
-        subjectPKInfo = #'CertificationRequestInfo_subjectPKInfo'{
-            subjectPublicKey =
+        subject = {'rdnSequence', []},
+        'subjectPKInfo' = #'CertificationRequestInfo_subjectPKInfo'{
+            'subjectPublicKey' =
                 public_key:der_encode(
                     'RSAPublicKey', #'RSAPublicKey'{
                         modulus = Key#'RSAPrivateKey'.modulus,
-                        publicExponent = Key#'RSAPrivateKey'.publicExponent
+                        'publicExponent' = Key#'RSAPrivateKey'.'publicExponent'
                     }
                 ),
             algorithm = #'CertificationRequestInfo_subjectPKInfo_algorithm'{
                 algorithm = ?rsaEncryption,
-                parameters = {asn1_OPENTYPE, <<5, 0>>}
+                parameters = {'asn1_OPENTYPE', <<5, 0>>}
             }
         },
         attributes = [A]
     },
     Csr = #'CertificationRequest'{
-        certificationRequestInfo = CsrI,
-        signatureAlgorithm = #'CertificationRequest_signatureAlgorithm'{
+        'certificationRequestInfo' = CsrI,
+        'signatureAlgorithm' = #'CertificationRequest_signatureAlgorithm'{
             algorithm = ?'sha256WithRSAEncryption'
         },
         signature =
